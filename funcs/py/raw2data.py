eeb04d42f096dacc04c8c6fee518a9e2e5b27c99
LHoogenboom <hoogenboom.l.p@gmail.com>
2021-04-23 18:02:31 +0200

    refactor raw2data into function

---
+++ funcs/py/raw2data.py
@@ -0,0 +1,25 @@
+def raw2data(source, targetPath,targetName):
+
+    import csv
+    import numpy as np
+
+    # with open('./raw/twoClass/Audio_data_two_class.csv','r') as csv_file: # point at csv file
+    with open(source,'r') as csv_file: # point at csv file
+
+        csv_reader = csv.reader(csv_file, delimiter=',') # reader for source csv
+        row = 0
+
+        for line in csv_reader: # every row
+
+            row+=1
+
+            track_data = line # audio data
+
+            with open(targetPath+targetName+str(row)+'.csv','w') as new_file: # writer for target csv
+                
+                print('tcTrack'+str(row)+'.csv') # log progress
+
+                # write row into separate file
+                csv_writer = csv.writer(new_file,delimiter=',')
+                csv_writer.writerow(track_data)
+
--- misc/pydeps.sh
+++ misc/pydeps.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 pip3 install numpy scipy matplotlib librosa librosa.display seaborn sklearn soundfile pandas
