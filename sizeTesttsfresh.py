42401ad69d2b573bb7732a28d75e76cd4b7c4075
LHoogenboom <hoogenboom.l.p@gmail.com>
2021-05-21 18:26:56 +0200

    tests tsfresh further with dask

--- sizeTesttsfresh.py
+++ sizeTesttsfresh.py
@@ -1,31 +1,45 @@
 from tsfresh import extract_features
 import pandas as pd
 import numpy as np
+import time as timer
+import dask
+import dask.dataframe as dd
+import dask.array as da
+
+start_time = timer.time()
 
 allTracks = np.load("./allTracks.npy")
 allTracks = np.transpose(allTracks)
+n = allTracks.shape[0]
+
+mod = np.zeros([2,n])
 
 
-mod = np.zeros([2,allTracks.shape[0]])
-
-
-time = np.linspace(0,allTracks.shape[0]-1,allTracks.shape[0])
+time = np.linspace(0,n-1,n)
 time = time.astype('int')
-idt = np.ones(allTracks.shape[0])
+idt = np.ones(n)
 
 mod[0,:] = time
 mod[1,:] = idt
 mod = mod.astype('int')
 mod = np.transpose(mod)
-a = allTracks[:,1:2]
+a = allTracks[0:n,0:1]
 npPre = np.append(mod,a,axis=1)
 print("Loading done")
-df = pd.DataFrame(npPre)
+
+
+df = dd.from_array(npPre)
+
+
+
 df = df.rename(columns={0:"time"})
 df = df.rename(columns={1:"id"})
+df = df.rename(columns={2:"track"})
+
 
 
 df.set_index("time")
 print("coversion done")
 df_features = extract_features(df, column_id="id", column_sort="time")
-print("done")
+print("\ndone")
+print("--- %s seconds ---" % (timer.time() - start_time))
--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-import timeFeatures as tf
+import dask
+import dask.dataframe as dd
+import dask.array as da
+import numpy as np
 
+allTracks = np.load("allTracks.npy")
+da = dd.from_array(allTracks[0,:])
+print("\n done")
+print(da.head())
